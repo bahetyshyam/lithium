921d1270bdea9114d359dcbc9eb4bf62b16e6cec
Sharath Satish <sharaths1998@gmail.com>
2019-10-08 22:40:18 +0530

    mobile.php

--- mobile.php
+++ mobile.php
@@ -13,6 +13,7 @@
 	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<link href="https://unpkg.com/ionicons@4.4.6/dist/css/ionicons.min.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Rubik&display=swap" rel="stylesheet">
 	<script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
 	<script type="text/javascript" src="js/materialize.js"></script>
 	<script type="text/javascript" src="js/fontawesome-all.min.js"></script>
@@ -44,17 +45,40 @@
 	    </nav>
     </div>
     
-    <div class="container">
-        <div class="row">
-            <div class="col m6 l6">
-                1st coloumn containing the image of the mobile
-            </div>
-            <div class="col m6 l6">
-                2nd coloumn containing the specs
-            </div>
-        </div>
+    <div class="mobileContainer">
+    	<?php
+			$mobileQuery = mysqli_query($con, "SELECT * FROM mobile WHERE mobile_id=2");
+			while ($row = mysqli_fetch_array($mobileQuery)) {
+
+if($row['mobile_soc'] == '')
+								echo "Hello World";
+				
+				echo "<div class='row'>
+						<div id='mobile-desc' class='col m7 l7'>
+							<img src=' ".  $row['mobile_picture'] ."'>
+							<h2>". $row['mobile_name'] ." </h2>
+						</div>
+						<div id='mobile-spec' class='col m5 l5 grey darken-4'>
+							<h2 align='center'>Specifications</h2>
+
+							<div class='spec-items'>
+								<p class='spec-desc'><i style='margin-right: 20px; font-size: 30px;' class='sideIcons fas fa-microchip'></i> ". $row['mobile_soc'] ."</p>
+							</div>
+							<div class='spec-items'>
+								<p class='spec-desc'><i style='margin-right: 20px; font-size: 30px;' class='sideIcons fas fa-mobile-alt'></i> ". $row['mobile_display'] ."</p>
+							</div>
+							<div class='spec-items'>
+								<p class='spec-desc'><i style='margin-right: 20px; font-size: 30px;' class='sideIcons fas fa-camera'></i> ". $row['mobile_camera'] ."</p>
+							</div>
+							<div class='spec-items'>
+								<p class='spec-desc'><i style='margin-right: 20px; font-size: 30px;' class='sideIcons fas fa-battery-three-quarters'></i> ". $row['mobile_battery'] ."</p>
+							</div>
+						</div>
+					</div>";
+			}
+
+		?>
     </div>
-    
 </body>
 <script type="text/javascript" src="js/index.js"></script>
 </html>
